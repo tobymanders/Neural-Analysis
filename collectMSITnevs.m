e0c23c3619c40b37fd9d76e3dc9797b10e1f9ea0
Toby Manders <trm2124@gmail.com>
2016-06-24 17:23:37 -0400

    Update collectMSITnevs.m

--- collectMSITnevs.m
+++ collectMSITnevs.m
@@ -1,15 +1,22 @@
 function [] = collectMSITnevs(patientID)
-%findMSITnevs This function scours the patient folder structure to find
-%MSIT data.
-%   Input should be a string with the patient ID, e.g. 'CUBF11'.
+%Finds MSIT data and copies .nev and .ns5 files to new folder
+%   Searches all subdirectories in patient directory for recordings with
+%   >100 trials of MSIT data, and copies relevant data for analysis.
 filepath = '/mnt/mfs/patients/BF/';
 cd([filepath patientID]);
 files = subdir ('*.nev');
-dest = [filepath 'MSITnevs'];
+dest = '/mnt/mfs/home/NIMASTER/ehs2149/TobyData/MSITnevs';
+n = 0;
 for x = 1:length(files)
-    NEV = openNEV ([files(x).name],'nosave', 'noread', 'nowarning');
-    trigs = double(NEV.Data.SerialDigitalIO.UnparsedData);
-    if sum(trigs==90) > 100
-        copyfile (files(x).name, dest);
+    if files(x).bytes >40000
+        NEV = openNEV ([files(x).name],'nosave', 'noread', 'nowarning');
+        trigs = double(NEV.Data.SerialDigitalIO.UnparsedData);
+        if sum(trigs==90) > 100
+            copyfile (files(x).name, dest);
+            copyfile ([files(x).name(1:end-3) 'ns5'], dest);
+            n = n+1;
+        end
+    else
     end
 end
+display(sprintf ('Found and moved %d recordings', n));
